6030e3425e18f7038fb8132f01b234bcbab5dd05
AthenasCode <emoore5203@gmail.com>
2023-03-01 15:09:04 +0800

    created react app & tailwind

---
+++ interactive-card-details-form/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full flex flex-col justify-center items-center">
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default App;
