31a8b9dea6adc885dd144eb836fbcaa7e95504fc
AthenasCode <emoore5203@gmail.com>
2023-03-01 20:26:18 +0800

    probably should have committed earlier. added cards basic styling and started introducing state

--- interactive-card-details-form/src/App.js
+++ interactive-card-details-form/src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
 import Form from "./components/form";
+import { useState } from "react";
 
 function App() {
+  const [card, setCard] = useState({
+    number: "0000 0000 0000 0000",
+    name: "Jane Appleseed",
+    date: "00/00",
+    cvc: "000",
+  });
+
   return (
     <>
       <div className="h-full grid grid-cols-3">
+        <div
+          className="absolute left-[17%] top-[23.5%] w-[447px] h-[245px]"
+          id="cardfront"
+        >
+          <div>{card.number}</div>
+          <div>{card.name}</div>
+          <div>{card.date}</div>
+        </div>
+        <div
+          id="cardback"
+          className="absolute left-[25%] bottom-[23.5%] w-[447px] h-[245px]"
+        >
+          <div>{card.cvc}</div>
+        </div>
+
         <div id="background-img" className="col-start-1 col-end-2"></div>
         <div className="col-start-2 col-end-4 h-full flex flex-col justify-center items-center">
-          <Form />
+          <Form cardNumber={card} setCardNumber={setCard} />
         </div>
       </div>
     </>
--- interactive-card-details-form/src/components/form.js
+++ interactive-card-details-form/src/components/form.js
@@ -2,33 +2,57 @@ export default function Form() {
   return (
     <div>
       <form className="flex flex-col">
-        <label htmlFor="name" className="text-blue-600">
+        <label htmlFor="name">
           CARDHOLDER NAME
           <br />
-          <input type="text" name="name" placeholder="e.g. Jane Appleseed" />
+          <input
+            className="w-full"
+            type="text"
+            name="name"
+            placeholder="e.g. Jane Appleseed"
+          />
         </label>
 
         <label htmlFor="number">
           CARD NUMBER
           <br />
           <input
+            className="w-full"
             type="text"
             name="number"
             placeholder="e.g. 1234 5678 9123 0000"
           />
         </label>
+        <div className="flex mb-5">
+          <label htmlFor="date">
+            EXP. DATE (MM/YY) <br />
+            <input
+              className="mr-2 w-20"
+              type="text"
+              name="date"
+              id="month"
+              placeholder="MM"
+            />
+            <input
+              className="mr-5 w-20"
+              type="text"
+              name="date"
+              id="year"
+              placeholder="YY"
+            />
+          </label>
 
-        <label htmlFor="date">
-          EXP. DATE (MM/YY) <br />
-          <input type="text" name="date" id="month" />
-          <input type="text" name="date" id="year" />
-        </label>
+          <label htmlFor="CVC">
+            CVC <br />
+            <input type="text" name="CVC" placeholder="e.g. 123" />
+          </label>
+        </div>
 
-        <label htmlFor="CVC">
-          CVC <br />
-          <input type="text" name="CVC" placeholder="e.g. 123" />
-        </label>
-        <button type="submit" className="bg-dark-violet text-white">
+        <button
+          type="submit"
+          id="submit-btn"
+          className="bg-dark-violet rounded-md py-2"
+        >
           Confirm
         </button>
       </form>
