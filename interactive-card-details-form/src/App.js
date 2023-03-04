87fdf1ef71ae00cd374abe422f5a020ab93f9e5c
AthenasCode <emoore5203@gmail.com>
2023-03-04 20:50:03 +0800

    positioned card details within card - not yet responsive

--- interactive-card-details-form/src/App.js
+++ interactive-card-details-form/src/App.js
@@ -38,7 +38,48 @@ function App() {
     <>
       <div className="h-full grid grid-cols-3">
         <div id="background-img" className="col-start-1 col-end-2"></div>
-        <div className="relative z-1000 col-start-2 col-end-4 h-full flex flex-col justify-center items-center">
+        <div
+          id="card-div"
+          className="text-white absolute w-2/3 h-full flex flex-col justify-center items-center m-auto col-start-1 col-end-2"
+        >
+          <div
+            className="relative bottom-5 right-20 shadow-subtle w-[25vw] min-w-[300px] aspect-[182/100] p-5 flex flex-col"
+            id="cardfront"
+          >
+            <div>Icon</div>
+            <div className="uppercase tracking-widest 2xl:text-2xl xl:text-[24px] mt-12">
+              {card.number ? card.number : "0000 0000 0000 0000"}
+            </div>
+            <div
+              id="name-and-date"
+              className="flex justify-between text-xs tracking-widest mt-4"
+            >
+              <div className="uppercase">
+                {card.name ? card.name : "Jane Appleseed"}
+              </div>
+              <div>
+                {card.month
+                  ? card.month.length < 2
+                    ? "0" + card.month
+                    : card.month
+                  : "00"}
+                /{card.year ? card.year : "00"}
+              </div>
+            </div>
+          </div>
+          <div
+            id="cardback"
+            className="relative top-5 shadow-subtle w-[25vw] aspect-[182/100]"
+          >
+            <div className="relative top-24 left-80">
+              {card.cvc ? card.cvc : "000"}
+            </div>
+          </div>
+        </div>
+        <div
+          id="ty-and-form-div"
+          className="relative z-1000 col-start-2 col-end-4 h-full flex flex-col justify-center items-center"
+        >
           {thankYou && (
             <ThankYou
               setThankYou={setThankYou}
@@ -62,34 +103,6 @@ function App() {
               setDisplayBlank={setDisplayBlank}
             />
           )}
-        </div>
-        <div className="text-white absolute z-1 w-2/3 h-full flex flex-col justify-center items-center m-auto col-start-1 col-end-2">
-          <div
-            className="relative bottom-5 right-20 shadow-subtle w-[25vw] aspect-[182/100]"
-            id="cardfront"
-          >
-            <div>Icon</div>
-            <div className="uppercase ">
-              {card.number ? card.number : "0000 0000 0000 0000"}
-            </div>
-            <div className="uppercase">
-              {card.name ? card.name : "Jane Appleseed"}
-            </div>
-            <div>
-              {card.month
-                ? card.month.length < 2
-                  ? "0" + card.month
-                  : card.month
-                : "00"}
-              /{card.year ? card.year : "00"}
-            </div>
-          </div>
-          <div
-            id="cardback"
-            className="relative top-5 shadow-subtle w-[25vw] aspect-[182/100]"
-          >
-            <div>{card.cvc ? card.cvc : "000"}</div>
-          </div>
         </div>
       </div>
     </>
